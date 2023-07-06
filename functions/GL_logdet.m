8bb058ce229a279b7e18897d4f3c446c8edc435b
Avrona <127651428+Mizera-Mondo@users.noreply.github.com>
2023-07-07 00:17:20 +0800

    feat:

---
+++ functions/GL_logdet.m
@@ -0,0 +1,11 @@
+function [outputArg1,outputArg2] = GL_logdet(D, options)
+%GL_LOGDET Graph learning with log det method.
+arguments
+    D double
+    options.beta = 14
+end
+% Initialization
+    % normalize data
+    
+end
+
