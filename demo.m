d4d19674d6da2e54ea2f1fa56ae2ecb4f11f5963
Avrona <127651428+Mizera-Mondo@users.noreply.github.com>
2023-07-12 01:07:52 +0800

    feat: finished GL_logdet.m

---
+++ demo.m
@@ -0,0 +1,3 @@
+addpath(genpath(pwd));
+D = randn(10, 100);
+Del = GL_logdet(D);
--- functions/GL_logdet.m
+++ functions/GL_logdet.m
@@ -1,13 +1,31 @@
-function [outputArg1,outputArg2] = GL_logdet(D, options)
-%GL_LOGDET Graph learning with log det method.
+function Del = GL_logdet(D, options)
+%GL_logdet Graph learning with log det method.
+% Ref: Discovering Structure by Learning Sparse Graphs
 arguments
     D double
     options.beta = 14
 end
 % Initialization
+    beta = options.beta;
     [n, m] = size(D);
+    on = ones(1, n);
+    I = eye(n);
+    maskW = ones(n) - eye(n);
+
     % normalize data
     D = normalize(D, 2, 'center');
     D = D./sqrt((max(abs(D*D'./m), [], 'all')));
+
+% Solving Structure Learning with CVX
+    cvx_begin
+        variable Del(n, n) semidefinite;
+        variable W(n, n) symmetric;
+        variable s nonnegative;
+        maximize log_det(Del) - trace(Del*(D*D')./m) - beta/m*norm(W, 1);
+        subject to
+            Del == diag(sum(W)) - W + I*s;
+            W.*I == 0;
+            W.*maskW >= 0;
+    cvx_end
 end
 
